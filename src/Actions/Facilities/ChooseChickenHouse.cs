432f42969eb8048e9f6ddb8b16b057979b4f4602
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-19 15:36:09 -0500

    Finished facility choosers that accept filtered lists

---
+++ src/Actions/Facilities/ChooseChickenHouse.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Trestlebridge.Interfaces;
+using Trestlebridge.Models;
+using Trestlebridge.Models.Animals;
+using Trestlebridge.Models.Facilities;
+
+namespace Trestlebridge.Actions
+{
+    public class ChooseChickenHouse
+    {
+        public static void CollectInput(List<ChickenHouse> houses, Chicken chicken)
+        {
+            Console.Clear();
+
+            for (int i = 0; i < houses.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. ChickenHouse: {houses[i].Name}");
+            }
+
+            Console.WriteLine();
+
+            // How can I output the type of animal chosen here?
+            Console.WriteLine("Place the chicken where?");
+
+            Console.Write("> ");
+            int choice = Int32.Parse(Console.ReadLine());
+
+            houses[choice - 1].AddResource(chicken);
+
+            /*
+                Couldn't get this to work. Can you?
+                Stretch goal. Only if the app is fully functional.
+             */
+            // farm.PurchaseResource<IGrazing>(animal, choice);
+
+        }
+    }
+}
---
+++ src/Actions/Facilities/ChooseDuckHouse.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Trestlebridge.Interfaces;
+using Trestlebridge.Models;
+using Trestlebridge.Models.Animals;
+using Trestlebridge.Models.Facilities;
+
+namespace Trestlebridge.Actions
+{
+    public class chooseDuckHouse
+    {
+        public static void CollectInput(List<DuckHouse> houses, Duck duck)
+        {
+            Console.Clear();
+
+            for (int i = 0; i < houses.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. DuckHouse: {houses[i].Name}");
+            }
+
+            Console.WriteLine();
+
+            // How can I output the type of animal chosen here?
+            Console.WriteLine($"Place the duck where?");
+
+            Console.Write("> ");
+            int choice = Int32.Parse(Console.ReadLine());
+
+            houses[choice - 1].AddResource(duck);
+
+            /*
+                Couldn't get this to work. Can you?
+                Stretch goal. Only if the app is fully functional.
+             */
+            // farm.PurchaseResource<IGrazing>(animal, choice);
+
+        }
+    }
+}
