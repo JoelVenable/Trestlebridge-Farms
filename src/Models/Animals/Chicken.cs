1e43659af03627082bdd39382b7ad4989aabeaab
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-23 15:22:45 -0500

    Fixed various meat processing errors

--- src/Models/Animals/Chicken.cs
+++ src/Models/Animals/Chicken.cs
@@ -4,43 +4,43 @@ using Trestlebridge.Interfaces;
 
 namespace Trestlebridge.Models.Animals
 {
-    public class Chicken : IResource, IMeatProducing, IFeatherProducing, IEggProducing
+  public class Chicken : IResource, IMeatProducing, IFeatherProducing, IEggProducing
+  {
+    private Guid _id = Guid.NewGuid();
+    private double _meatProduced = 1.7;
+
+    private double _feathersProduced = .5;
+
+    private int _eggsProduced = 7;
+
+    private string _shortId
     {
-        private Guid _id = Guid.NewGuid();
-        private double _meatProduced = 1.7;
-
-        private double _feathersProduced = .5;
-
-        private int _eggsProduced = 7;
-
-        private string _shortId
-        {
-            get
-            {
-                return this._id.ToString().Substring(this._id.ToString().Length - 6);
-            }
-        }
-        public string Type { get; } = "Chicken";
-
-        public double Butcher()
-        {
-            return _meatProduced;
-        }
-
-        public int Gather()
-        {
-            throw new NotImplementedException();
-        }
-
-        public double Pluck()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override string ToString()
-        {
-            return "The Chicken goes: Cluck!";
-        }
+      get
+      {
+        return this._id.ToString().Substring(this._id.ToString().Length - 6);
+      }
     }
+    public string Type { get; } = "Chicken";
+
+    public double Butcher()
+    {
+      return _meatProduced;
+    }
+
+    public int Gather()
+    {
+      throw new NotImplementedException();
+    }
+
+    public double Pluck()
+    {
+      return _feathersProduced;
+    }
+
+    public override string ToString()
+    {
+      return "The Chicken goes: Cluck!";
+    }
+  }
 
 }
--- src/Models/Animals/Duck.cs
+++ src/Models/Animals/Duck.cs
@@ -3,48 +3,54 @@ using Trestlebridge.Interfaces;
 
 namespace Trestlebridge.Models.Animals
 {
-    public class Duck : IResource, IEggProducing, IFeatherProducing, IMeatProducing
+  public class Duck : IResource, IEggProducing, IFeatherProducing, IMeatProducing
+  {
+    private Guid _id = Guid.NewGuid();
+
+    private int _eggsProduced = 6;
+
+    private double _meatProduced = 1.2;
+
+    private double _feathersProduced = 0.75;
+
+    private string _shortId
     {
-        private Guid _id = Guid.NewGuid();
-
-        private int _eggsProduced = 6;
-
-        private double _meatProduced = 1.2;
-
-        private double _feathersProduced = 0.75;
-
-        private string _shortId
-        {
-            get
-            {
-                return this._id.ToString().Substring(this._id.ToString().Length - 6);
-            }
-        }
-        public double GrassPerDay { get; set; } = 0.8;
-
-
-        public string Type { get; } = "Duck";
-
-
-        public int Gather()
-        {
-            return 6;
-        }
-
-        public double Butcher()
-        {
-            return _meatProduced;
-        }
-
-
-        double IFeatherProducing.Pluck()
-        {
-            return 0.75;
-        }
-
-        public override string ToString()
-        {
-            return "The Duck goes Quack!";
-        }
+      get
+      {
+        return this._id.ToString().Substring(this._id.ToString().Length - 6);
+      }
     }
+    public double GrassPerDay { get; set; } = 0.8;
+
+
+    public string Type { get; } = "Duck";
+
+
+    public int Gather()
+    {
+      return 6;
+    }
+
+    public double Butcher()
+    {
+      return _meatProduced;
+    }
+
+    public double Pluck()
+    {
+      return _feathersProduced;
+    }
+
+
+
+    double IFeatherProducing.Pluck()
+    {
+      return 0.75;
+    }
+
+    public override string ToString()
+    {
+      return "The Duck goes Quack!";
+    }
+  }
 }
