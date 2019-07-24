757a7744207847b7cb5780a74b37376833870627
Sean Glavin <sean@Seans-MacBook-Pro.local>
2019-07-24 10:54:46 -0500

    new master

--- src/Actions/Facilities/ChooseDuckHouse.cs
+++ src/Actions/Facilities/ChooseDuckHouse.cs
@@ -8,41 +8,41 @@ using Trestlebridge.Models.Facilities;
 
 namespace Trestlebridge.Actions
 {
-  public class chooseDuckHouse
-  {
-    public static void CollectInput(List<DuckHouse> houses, Duck duck)
+    public class chooseDuckHouse
     {
-      Console.Clear();
+        public static void CollectInput(List<DuckHouse> houses, Duck duck)
+        {
+            Console.Clear();
 
-      for (int i = 0; i < houses.Count; i++)
-      {
-        Console.WriteLine($"{i + 1}. Duck House: {houses[i].Name} ({houses[i].NumAnimals} animals)");
-      }
+            for (int i = 0; i < houses.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. Duck House: {houses[i].Name} ({houses[i].NumAnimals} animals)");
+            }
 
-      Console.WriteLine();
+            Console.WriteLine();
 
-      // How can I output the type of animal chosen here?
-      Console.WriteLine($"Place the duck where?");
+            // How can I output the type of animal chosen here?
+            Console.WriteLine($"Place the duck where?");
 
-      Console.Write("> ");
-      try
-      {
-        int choice = Int32.Parse(Console.ReadLine());
+            Console.Write("> ");
+            try
+            {
+                int choice = Int32.Parse(Console.ReadLine());
 
-        houses[choice - 1].AddResource(duck);
+                houses[choice - 1].AddResource(duck);
 
-      }
-      catch (Exception ex)
-      {
-        Program.ShowMessage("Invalid Input");
-      }
+            }
+            catch (Exception)
+            {
+                Program.ShowMessage("Invalid Input");
+            }
 
-      /*
-          Couldn't get this to work. Can you?
-          Stretch goal. Only if the app is fully functional.
-       */
-      // farm.PurchaseResource<IGrazing>(animal, choice);
+            /*
+                Couldn't get this to work. Can you?
+                Stretch goal. Only if the app is fully functional.
+             */
+            // farm.PurchaseResource<IGrazing>(animal, choice);
 
+        }
     }
-  }
 }
--- src/Actions/PurchaseSeed.cs
+++ src/Actions/PurchaseSeed.cs
@@ -11,52 +11,77 @@ namespace Trestlebridge.Actions
     {
         public static void CollectInput(Farm farm)
         {
-            Console.WriteLine("1. Sesame");
-            Console.WriteLine("2. Wildflower");
-            Console.WriteLine("3. Sunflower");
 
-            Console.WriteLine();
-            Console.WriteLine("What seed are you buying today?");
-
-            Console.Write("> ");
-            string choice = Console.ReadLine();
-
-            switch (Int32.Parse(choice))
+            bool doOver;
+            do
             {
-                case 1:
-                    ChoosePlowedField.CollectInput(farm, new Sesame());
-                    break;
-                case 2:
-                    ChooseNaturalField.CollectInput(farm, new Wildflower());
-                    break;
-                case 3:
-                    {
-                        Console.Clear();
-                        Program.DisplayBanner();
-                        Console.WriteLine("1. Natural Field");
-                        Console.WriteLine("2. Plowed Field");
-                        Console.WriteLine();
-                        Console.WriteLine("Choose What type of Field to plant your Sunflowers in:");
-                        Console.Write("> ");
-                        string fieldType = Console.ReadLine();
+                Console.WriteLine("1. Sesame");
+                Console.WriteLine("2. Wildflower");
+                Console.WriteLine("3. Sunflower");
 
-                        switch (Int32.Parse(fieldType))
-                        {
-                            case 1:
-                                ChooseNaturalField.CollectInput(farm, new Sunflower());
-                                break;
-                            case 2:
-                                ChoosePlowedField.CollectInput(farm, new Sunflower());
-                                break;
-                            default:
-                                break;
+                Console.WriteLine();
+                Console.WriteLine("What seeds are you buying today?");
 
-                        }
+                Console.Write("> ");
+                string choice = Console.ReadLine();
+
+                doOver = false;
+
+                int parsedChoice;
+                try
+                {
+
+                    parsedChoice = Int32.Parse(choice);
+                }
+                catch (Exception)
+                {
+                    parsedChoice = 0;
+                }
+
+                switch (parsedChoice)
+                {
+                    case 1:
+                        ChoosePlowedField.CollectInput(farm, new Sesame());
                         break;
-                    }
-                default:
-                    break;
-            }
+                    case 2:
+                        ChooseNaturalField.CollectInput(farm, new Wildflower());
+                        break;
+                    case 3:
+                        {
+                            Console.Clear();
+                            Program.DisplayBanner();
+                            Console.WriteLine("1. Natural Field");
+                            Console.WriteLine("2. Plowed Field");
+                            Console.WriteLine();
+                            Console.WriteLine("Choose What type of Field to plant your Sunflowers in:");
+                            Console.Write("> ");
+                            string fieldType = Console.ReadLine();
+
+                            switch (Int32.Parse(fieldType))
+                            {
+                                case 1:
+                                    ChooseNaturalField.CollectInput(farm, new Sunflower());
+                                    break;
+                                case 2:
+                                    ChoosePlowedField.CollectInput(farm, new Sunflower());
+                                    break;
+                                default:
+                                    break;
+
+                            }
+                            break;
+                        }
+                    default:
+                        Program.ShowMessage("Invalid selection.  Please choose again.");
+                        doOver = true;
+                        break;
+                }
+            } while (doOver);
+        }
+
+        internal static void CollectInput(object trestlebridge)
+        {
+            throw new NotImplementedException();
         }
     }
 }
